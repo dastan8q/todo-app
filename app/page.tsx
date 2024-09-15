a56b77ac8f988d427188018cfd67ff2fc93983cc
Mario <mariofenandosantacruzp@gmail.com>
2024-09-15 17:51:47 -0500

    Proyecto todo-app inicializado

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,85 @@
+"use client";
 import Image from "next/image";
+import { useState, ChangeEvent, KeyboardEvent } from 'react';
+
+interface TodoItem {
+  id: string;
+  content: string;
+}
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [todo, setTodo] = useState<TodoItem[]>([]);
+  const [newTodo, setNewTodo] = useState<string>('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const getRandomNumber = (): number => {
+    return Math.floor(Math.random() * 9999);
+  };
+
+  const handleKeyUp = (key: string) => {
+    if (key === 'Enter' && newTodo) {
+      const randomNumber = getRandomNumber();
+
+      const newItem: TodoItem = {
+        id: `item-${randomNumber}`,
+        content: newTodo,
+      };
+
+      setTodo(todo.concat(newItem));
+      setNewTodo('');
+    }
+  };
+
+  const handleDelete = (id: number) => {
+    if (id > -1) {
+      setTodo(todo.slice(0, id).concat(todo.slice(id + 1)));
+    }
+  };
+
+  return (
+    <div className="flex justify-center pt-40">
+      <div className="max-w-sm w-full shadow-lg bg-white p-8 rounded-xl opacity-70">
+        <div className="flex justify-center cursor-default bg-gray-200 rounded-3xl px-4 py-1 color-gray hover:scale-110 transition-all">
+          <img className="object-cover rounded-full w-16 h-16 m-2" src="https://i.pinimg.com/originals/42/9b/b4/429bb47fcda0b84f88ec04fbe9ac2328.jpg" alt="theprince" />
+          <div className="w-full p-3">
+            <p className="text-3xl text-gray-600">My To-do list</p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+
+        <div className="relative mt-10">
+          <div className="absolute inset-y-0 left-2 flex items-center pl-3 pointer-events-none">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus-circle" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="newTodo"
+            value={newTodo}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => setNewTodo(e.target.value)}
+            onKeyUp={(e: KeyboardEvent<HTMLInputElement>) => handleKeyUp(e.key)}
+            className="block w-full pl-10 p-2 border-4 rounded-full bg-gray-600 text-white"
+            placeholder="Nueva tarea"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+        <ul className="block w-full pt-6">
+          {todo?.map((item, index) => {
+            return (
+              <li key={item.id} className="w-full border-2 rounded-xl mt-2 hover:border-blue-300">
+                <input id={`checkbox-${index}`} type="checkbox" className="float-left block w-6 h-6 m-3" />
+                <button id={`button-${index}`} onClick={() => handleDelete(index)} className="float-right w-7 h-7 m-2.5 rounded-2xl bg-red-700 text-gray-200 shadow-md hover:bg-red-500 hover:scale-105">
+                  x
+                </button>
+                <label htmlFor={`checkbox-${index}`} className="block w-full p-3">
+                  {item.content}
+                </label>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
+
